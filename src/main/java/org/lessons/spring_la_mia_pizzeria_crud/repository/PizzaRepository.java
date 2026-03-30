d62c5f39b62af257030d724b278ad453a50fdd9c
Anniballi-Andrea <andreanniballi@gmail.com>
2026-03-30 10:20:52 +0200

    apply same change

--- src/main/java/org/lessons/spring_la_mia_pizzeria_crud/repository/PizzaRepository.java
+++ src/main/java/org/lessons/spring_la_mia_pizzeria_crud/repository/PizzaRepository.java
@@ -8,4 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface PizzaRepository extends JpaRepository<Pizza, Integer> {
 
     public List<Pizza> findByNameContaining(String name);
+
 }
